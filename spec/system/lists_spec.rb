0b6d22da52a22e4477988ed1587be2ef96ba4ce6
rata854 <nitiham2006@gmail.com>
2021-05-24 00:39:51 +0000

    [Add]テストコード

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -15,8 +15,8 @@ describe '投稿のテスト' do
     it 'top_pathが"/top"であるか' do
       expect(current_path).to eq('/top')
     end
-   end
- end
+  end
+end
 end
 
 describe '投稿画面のテスト' do
@@ -53,3 +53,54 @@ describe '投稿一覧のテスト' do
     end
   end
 end
+
+describe '詳細画面のテスト' do
+  before '詳細画面への遷移' do
+    visit show_todolist_path
+  end
+  context '表示のテスト' do
+    it '削除リンクが存在しているか' do
+      expect(page).to have_link '削除'
+    end
+    it '編集リンクが存在しているか' do
+      expect(page).to have_link '編集'
+    end
+  end
+  context 'リンクの遷移先の確認' do
+    it '編集の遷移先は編集画面か' do
+      edit_link = find_all('a')[0]
+      edit_link.click
+      expect(current_path).to eq('/todolists/' + list.id.to_s + '/edit')
+    end
+  end
+  context 'list削除のテスト' do
+    it 'listの削除' do
+      expect{ todolist.destroy }.to change{ List.count }.by(-1)
+    end
+  end
+end
+
+describe '編集画面のテスト' do
+  before '編集画面への遷移' do
+    visit edit_todolist_path
+  end
+  context '表示の確認' do
+    it '編集前のタイトルと本文がフォームに表示（セット）されてるか' do
+      expect(page).to have_field 'list[title]', with: list.title
+      expect(page).to have_field 'list[body]', with: list.body
+    end
+    it '保存ボタンが表示される' do
+      expect(page).to have_button '保存'
+    end
+  end
+  context '更新処理に関するテスト' do
+    it '更新後のリダイレクト先は正しいか' do
+      fill_in 'list[title]', with: Faker::Lorem.characters(number:5)
+      fill_in 'list[body]', with: Faker::Lorem.characters(number:20)
+      click_button '保存'
+      expect(page).to have_current_path todolist_path(list)
+    end
+  end
+end
+
+
